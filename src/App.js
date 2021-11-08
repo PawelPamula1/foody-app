a4eab9110bc4bdb2dccb38e7f0074fffeafdd9d7
PawelPamula1 <pawelpamula003@gmail.com>
2021-11-08 16:11:30 +0100

    fetching mealList data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React, { useState } from 'react';
+import MealList from './MealList';
 
 function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  const handleChange = (e) => {
+    setCalories(e.target.value);
+  };
+
+  const getMealData = () => {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=9b076a7b0bf948fa81036292fbaf30e9&timeFrame=day&targetCalories=${calories}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMealData(data);
+      })
+      .catch(() => {
+        console.log('error');
+      });
+  };
+
   return (
     <div className="App">
-      <p>Siemaneczko</p>
+      <section className="controls">
+        <input
+          type="number"
+          placeholder="Calories (e.g. 2000)"
+          onChange={handleChange}
+        />
+      </section>
+      <button onClick={getMealData}>Get Daily Meal Plan</button>
+      {mealData && <MealList mealData={mealData} />}
     </div>
   );
 }
---
+++ src/Meal.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const Meal = ({ meal }) => {
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    fetch(
+      `https://api.spoonacular.com/recipes/${meal.id}/information?apiKey=9b076a7b0bf948fa81036292fbaf30e9&includeNutrition=false`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImageUrl(data.image);
+      })
+      .catch(() => {
+        console.log('error');
+      });
+  }, [meal.id]);
+
+  return (
+    <article>
+      <h1>{meal.title}</h1>
+      <img src={imageUrl} alt="recipe" />
+      <ul>
+        <li>Preparation time: {meal.readyInMinutes} minutes</li>
+        <li>Number of servings: {meal.servings}</li>
+      </ul>
+
+      <a href={meal.sourceUrl}>Go to Recipe</a>
+    </article>
+  );
+};
+
+export default Meal;
