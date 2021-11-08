3c6edde6d8c5db1e1e38ed044958d329e1e44c94
PawelPamula1 <pawelpamula003@gmail.com>
2021-11-08 17:37:37 +0100

    error message handling

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import MealList from './MealList';
 function App() {
   const [mealData, setMealData] = useState(null);
   const [calories, setCalories] = useState(2000);
+  const [isError, setIsError] = useState(false);
 
   const handleChange = (e) => {
     setCalories(e.target.value);
   };
 
-  const getMealData = (e) => {
+  const getMealData = async (e) => {
     e.preventDefault();
-    fetch(
+    const response = await fetch(
       `https://api.spoonacular.com/mealplanner/generate?apiKey=9b076a7b0bf948fa81036292fbaf30e9&timeFrame=day&targetCalories=${calories}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setMealData(data);
-      })
-      .catch(() => {
-        console.log('error');
-      });
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      setMealData(data);
+    } else {
+      setIsError(true);
+    }
+
+    console.log(data);
   };
 
   return (
@@ -37,6 +40,12 @@ function App() {
         <button>Get Daily Meal Plan</button>
       </form>
       {mealData && <MealList mealData={mealData} />}
+      {isError && (
+        <p className="error">
+          My daily points limit on this site has been reached. In order to make
+          this app work try tommorrow. Sorry.
+        </p>
+      )}
     </div>
   );
 }
